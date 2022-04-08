21b2c709c0306ac8755d7cccbe8b34cde3ff19b8
Cherryleafroad <13651622+cherryleafroad@users.noreply.github.com>
2022-04-08 15:32:48 -0700

    Fix filename extensions

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,23 @@ fn main() {
     for s in all_strings {
         if s.0.contains(".vst3") ||
            s.0.contains(".dll") {
-            println!("{}", s.0);
+
+            let plugin = s.0;
+            let matchs;
+            let ext;
+            if plugin.contains(".vst3") {
+                matchs = plugin.rmatch_indices("vst3").collect::<Vec<_>>()[0];
+                ext = ".vst3";
+            } else {
+                matchs = plugin.rmatch_indices("dll").collect::<Vec<_>>()[0];
+                ext = ".dll";
+            }
+
+            // slice at index, then tack on the end
+            let base = &plugin[..matchs.0-1];
+            let plugin = format!("{base}{ext}");
+
+            println!("{plugin}");
         }
     }
 }
