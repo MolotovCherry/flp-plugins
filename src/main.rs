9a00e1b9f187503cf9f4ef2d907d895141599526
Ewedu_leaf <120208123+ewedusoft@users.noreply.github.com>
2024-05-02 09:44:28 +0100

    Update main.rs to add ability to read CLAP plugins
    Since FL is adding the ability to use CLAP plugins, I thought I'd make a quick edit to enable it to read CLAP plugins on file, too.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,8 @@ fn main() {
 
     for s in all_strings {
         if s.0.contains(".vst3") ||
-           s.0.contains(".dll") {
+           s.0.contains(".dll") ||
+           s.0.contains(".clap") {
 
             let plugin = s.0;
             let matchs;
@@ -23,9 +24,12 @@ fn main() {
             if plugin.contains(".vst3") {
                 matchs = plugin.rmatch_indices("vst3").collect::<Vec<_>>()[0];
                 ext = ".vst3";
-            } else {
+            } else if plugin.contains(".dll") {
                 matchs = plugin.rmatch_indices("dll").collect::<Vec<_>>()[0];
                 ext = ".dll";
+            } else {
+                matchs = plugin.rmatch_indices("clap").collect::<Vec<_>>()[0];
+                ext = ".clap";
             }
 
             // slice at index, then tack on the end
